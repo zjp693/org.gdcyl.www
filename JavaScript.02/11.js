2bc9c1fe1450f8c24d95e734a01f2a2212e92605
zjp639 <942680978@qq.com>
2019-09-24 23:09:17 +0800

    day9.txt

--- JavaScript.02/11.js
+++ JavaScript.02/11.js
@@ -1,3 +1,4 @@
+//定义一个数组，里面的元素要把今天所学的数据类型都用到
 // 基本数据类型:Number(整数，小数),string(字符串),undefined(未定义),null,boolean(布尔)
 // 引用数据类型:object(对象),Array(数组),Symbol
 
--- JavaScript.02/12  13.js
+++ JavaScript.02/12  13.js
@@ -1,4 +1,10 @@
 
+//定义一个数组，数组里面放2个对象，对象的属性有name,age,gender,studentId,属性值自己写
+//要求：
+//1.输出第二个对象里面的属性name的值
+//2.将第一个对象里面的属性gender的值修改成数字18
+//3.算出2个对象里面属性是age的和
+//13.定义一个数组，数组里面有一个对象，对象里面有属性price,num,name,colors,colors的值是一个数组，获取colors数组下标是1的值
 var arr = [
     {
         name : 'nnnn',
