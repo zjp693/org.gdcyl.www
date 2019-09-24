6daa2c9191c77fc4e2545207ad24a00af80fc7a5
zjp639 <942680978@qq.com>
2019-09-24 22:55:07 +0800

    day9.txt

--- JavaScript.02/12  13.js
+++ JavaScript.02/12  13.js
@@ -15,13 +15,10 @@ var arr = [
     }
 ]
 
-// 输出第二个对象里面name的值
 console.log(arr[1].name);
 
-// 将第一个对象gender的属性改为18
 arr[0].gender = 18;
 
-// 算出两个age的和
 var sum = arr[0].age + arr[1].age;
 
 console.log(sum);
